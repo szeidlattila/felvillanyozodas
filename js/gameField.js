4c6129c135a17fc58e38d5e346fcfb324f5407b2
Szeidl Attila <a22oy5@inf.elte.hu>
2024-02-20 15:16:09 +0100

    upload existing project

---
+++ js/gameField.js
@@ -0,0 +1,416 @@
+let gameField = document.querySelector("#game-field");
+let table = document.querySelector("#tables");
+
+table.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("black")) {
+    if (e.target.innerHTML === "💡") {
+      turnOffLight(e.target);
+    } else {
+      turnOnLight(e.target);
+    }
+    BlackFieldWithNumberNextTo(e.target);
+    isCorrect();
+  }
+});
+
+function turnOffLight(t) {
+  // t := e.target
+  t.innerHTML = "";
+  if (t.classList.contains("wrong")) t.classList.remove("wrong"); // ha piros leveszi rola
+
+  if (!isLightBulbHorizontal(t) || !isLightBulbVertical(t)) {
+    if (!isLightBulbHorizontal(t) && !isLightBulbVertical(t))
+      t.classList.remove("light");
+
+    if (!isLightBulbHorizontal(t)) {
+      // balra
+      let field = t;
+      while (left(field) !== null && !left(field).classList.contains("black")) {
+        field = left(field);
+        if (!isLightBulbVertical(field)) field.classList.remove("light");
+      }
+
+      // jobbra
+      field = t;
+      while (
+        right(field) !== null &&
+        !right(field).classList.contains("black")
+      ) {
+        field = right(field);
+        if (!isLightBulbVertical(field)) field.classList.remove("light");
+      }
+    }
+
+    if (!isLightBulbVertical(t)) {
+      // fel
+      field = t;
+      while (
+        above(field) !== null &&
+        !above(field).classList.contains("black")
+      ) {
+        field = above(field);
+        if (!isLightBulbHorizontal(field)) field.classList.remove("light");
+      }
+
+      // le
+      field = t;
+      while (
+        below(field) !== null &&
+        !below(field).classList.contains("black")
+      ) {
+        field = below(field);
+        if (!isLightBulbHorizontal(field)) field.classList.remove("light");
+      }
+    }
+  }
+  // piros mezok eltuntetese, ha szukseges
+  if (
+    isOnlyOneLightBulbHorizontal(t) !== false ||
+    isOnlyOneLightBulbVertical(t) !== false
+  ) {
+    removeWrong(t);
+  }
+}
+
+function turnOnLight(t) {
+  // t := e.target
+  t.innerHTML = "💡";
+  t.classList.add("light");
+
+  // balra
+  let field = t;
+  while (left(field) !== null && !left(field).classList.contains("black")) {
+    field = left(field);
+    if (field.innerHTML === "💡") {
+      t.classList.add("wrong");
+      field.classList.add("wrong");
+    }
+    field.classList.add("light")
+  }
+
+  // jobbra
+  field = t;
+  while (right(field) !== null && !right(field).classList.contains("black")) {
+    field = right(field);
+    if (field.innerHTML === "💡") {
+      t.classList.add("wrong");
+      field.classList.add("wrong");
+    }
+    field.classList.add("light")
+  }
+
+  // fel
+  field = t;
+  while (above(field) !== null && !above(field).classList.contains("black")) {
+    field = above(field);
+    if (field.innerHTML === "💡") {
+      t.classList.add("wrong");
+      field.classList.add("wrong");
+    }
+    field.classList.add("light")
+  }
+
+  // le
+  field = t;
+  while (below(field) !== null && !below(field).classList.contains("black")) {
+    field = below(field);
+    if (field.innerHTML === "💡") {
+      t.classList.add("wrong");
+      field.classList.add("wrong");
+    }
+    field.classList.add("light")
+  }
+}
+
+function isLightBulbHorizontal(t) {
+  let field = t;
+  while (left(field) !== null && !left(field).classList.contains("black")) {
+    field = left(field);
+    if (field.innerHTML === "💡") return true;
+  }
+
+  field = t;
+  while (right(field) !== null && !right(field).classList.contains("black")) {
+    field = right(field);
+    if (field.innerHTML === "💡") return true;
+  }
+
+  return false;
+}
+
+function isOnlyOneLightBulbHorizontal(t) {
+  let count = 0;
+  let retField = null;
+  let field = t;
+  while (left(field) !== null && !left(field).classList.contains("black")) {
+    field = left(field);
+    if (field.innerHTML === "💡") {
+      count++;
+      retField = field;
+      if (count > 1) return false;
+    }
+  }
+
+  field = t;
+  while (right(field) !== null && !right(field).classList.contains("black")) {
+    field = right(field);
+    if (field.innerHTML === "💡") {
+      count++;
+      retField = field;
+      if (count > 1) return false;
+    }
+  }
+
+  return retField;
+}
+
+function isLightBulbVertical(t) {
+  let field = t;
+  while (above(field) !== null && !above(field).classList.contains("black")) {
+    field = above(field);
+    if (field.innerHTML === "💡") {
+      return true;
+    }
+  }
+
+  field = t;
+  while (below(field) !== null && !below(field).classList.contains("black")) {
+    field = below(field);
+    if (field.innerHTML === "💡") {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function isOnlyOneLightBulbVertical(t) {
+  let count = 0;
+  let field = t;
+  while (above(field) !== null && !above(field).classList.contains("black")) {
+    field = above(field);
+    if (field.innerHTML === "💡") count++;
+    if (count > 1) return false;
+  }
+
+  field = t;
+  while (below(field) !== null && !below(field).classList.contains("black")) {
+    field = below(field);
+    if (field.innerHTML === "💡") count++;
+    if (count > 1) return false;
+  }
+
+  return true;
+}
+
+function left(t) {
+  return t.previousElementSibling;
+}
+
+function right(t) {
+  return t.nextElementSibling;
+}
+
+function above(t) {
+  if (
+    t.parentElement !== null &&
+    t.parentElement.previousElementSibling !== null &&
+    t.parentElement.previousElementSibling.firstChild !== null
+  ) {
+    let aboveField =
+      t.parentElement.previousElementSibling.firstChild.nextElementSibling;
+
+    for (let i = 0; i < findIndex(t); i++) {
+      aboveField = aboveField.nextElementSibling;
+    }
+
+    return aboveField;
+  }
+
+  return null;
+}
+
+function below(t) {
+  if (
+    t.parentElement !== null &&
+    t.parentElement.nextElementSibling !== null &&
+    t.parentElement.nextElementSibling.firstChild !== null
+  ) {
+    let belowField =
+      t.parentElement.nextElementSibling.firstChild.nextElementSibling;
+
+    for (let i = 0; i < findIndex(t); i++) {
+      belowField = belowField.nextElementSibling;
+    }
+
+    return belowField;
+  }
+
+  return null;
+}
+
+function findIndex(t) {
+  let leftField = t;
+
+  let index = 0;
+  while (leftField.previousElementSibling !== null) {
+    index++;
+    leftField = leftField.previousElementSibling;
+  }
+
+  return index;
+}
+
+function removeWrong(t) {
+  let field = t;
+  while (left(field) !== null && !left(field).classList.contains("black")) {
+    field = left(field);
+    if (!isLightBulbHorizontal(field) && !isLightBulbVertical(field)) {
+      field.classList.remove("wrong");
+    }
+  }
+
+  field = t;
+  while (right(field) !== null && !right(field).classList.contains("black")) {
+    field = right(field);
+    if (!isLightBulbHorizontal(field) && !isLightBulbVertical(field)) {
+      field.classList.remove("wrong");
+    }
+  }
+
+  field = t;
+  while (above(field) !== null && !above(field).classList.contains("black")) {
+    field = above(field);
+    if (!isLightBulbHorizontal(field) && !isLightBulbVertical(field)) {
+      field.classList.remove("wrong");
+    }
+  }
+
+  field = t;
+  while (below(field) !== null && !below(field).classList.contains("black")) {
+    field = below(field);
+    if (!isLightBulbHorizontal(field) && !isLightBulbVertical(field)) {
+      field.classList.remove("wrong");
+    }
+  }
+}
+
+function BlackFieldWithNumberNextTo(t) {
+  let field = t;
+  if (
+    left(field) !== null &&
+    left(field).classList.contains("black") &&
+    left(field).innerHTML !== ""
+  ) {
+    BlackFieldWithNumberCheck(left(field));
+  }
+
+  field = t;
+  if (
+    right(field) !== null &&
+    right(field).classList.contains("black") &&
+    right(field).innerHTML !== ""
+  ) {
+    BlackFieldWithNumberCheck(right(field));
+  }
+
+  field = t;
+  if (
+    above(field) !== null &&
+    above(field).classList.contains("black") &&
+    above(field).innerHTML !== ""
+  ) {
+    BlackFieldWithNumberCheck(above(field));
+  }
+
+  field = t;
+  if (
+    below(field) !== null &&
+    below(field).classList.contains("black") &&
+    below(field).innerHTML !== ""
+  ) {
+    BlackFieldWithNumberCheck(below(field));
+  }
+}
+
+function BlackFieldWithNumberCheck(t) {
+  let count = 0;
+
+  let field = t;
+  if (left(field) !== null && left(field).innerHTML === "💡") {
+    count++;
+  }
+
+  field = t;
+  if (right(field) !== null && right(field).innerHTML === "💡") {
+    count++;
+  }
+
+  field = t;
+  if (above(field) !== null && above(field).innerHTML === "💡") {
+    count++;
+  }
+
+  field = t;
+  if (below(field) !== null && below(field).innerHTML === "💡") {
+    count++;
+  }
+
+  if (count != t.innerHTML) {
+    // ha nem megfelelo szamu villanykorte van
+    t.style.color = "red";
+  } else {
+    t.style.color = "";
+  }
+}
+
+// beszinezi pirosra a nem 0 szammal ellatott fekete mezoket:
+let blackFields = [...table.querySelectorAll(".black")]
+blackFields
+  .filter((e) => e.innerHTML !== "" && e.innerHTML !== "0")
+  .map((e) => (e.style.color = "red"));
+
+// ellenorzes:
+function isCorrect() {
+  blackFields = [...table.querySelectorAll(".black")]
+  let myBlackFields = blackFields.filter(e => !e.classList.contains("inactive"))
+  if (myBlackFields.every((e) => e.style.color !== "red")) {
+    let wrongCells = [...table.querySelectorAll(".wrong")].filter(e => !e.classList.contains("inactive"))
+    if (wrongCells.length === 0) {
+      let lightCells = [...table.querySelectorAll(".light")].filter(e => !e.classList.contains("inactive"))
+      if (
+        lightCells.length + myBlackFields.length ===
+        fieldsCount
+      ) {
+        end();
+      }
+    }
+  }
+}
+
+// jatek vege: nyert
+function end() {
+  store();
+  stopTimer();
+  console.log("Nyert!");
+  btnSave.style.display = "none";
+  winText.style.display = "table";
+  if (saved) {
+    localStorage.clear();
+    previousGame.innerHTML = "Nincs mentett játék!"
+    btnContinue.disabled = true;
+  }
+  btnDelete.style.display = "block"
+}
+
+// local storageba helyezes
+function store() {
+  let datas = `${text.innerHTML}: ${nickname.innerHTML} - ${currentTime} másodperc`
+  let results = []
+  if (JSON.parse(localStorage.getItem("results")) !== null) {
+    results = JSON.parse(localStorage.getItem("results"));
+  }
+  results.unshift(datas);
+  localStorage.setItem("results", JSON.stringify(results))
+  document.querySelector("#results").innerHTML = results.join("<br />");
+}
---
+++ js/gameSelector.js
@@ -0,0 +1,330 @@
+const displayGameSelector = document.querySelector("#display-game-selector");
+const displayGameField = document.querySelector("#display-game-field");
+const tables = [...document.querySelectorAll("table")];
+const easyTable = document.querySelector("#table-easy");
+const advancedTable = document.querySelector("#table-advanced");
+const extremeTable = document.querySelector("#table-extreme")
+
+const text = document.querySelector("#text")
+const nickname = document.querySelector("#nickname")
+const winText = document.querySelector("#win")
+let time = document.querySelector("#time")
+let currentTime = 0;
+let timer = null
+
+const btnSave = document.querySelector("#save");
+const btnRestart = document.querySelector("#restart");
+const btnExit = document.querySelector("#exit");
+const btnContinue = document.querySelector("#continue-btn")
+const btnDelete = document.querySelector("#delete-results")
+
+let isEasy = false;
+let isAdvanced = false;
+let isExtreme = false;
+
+const btnEasy = document.querySelector("#easy");
+const displayEasy = document.querySelector("#display-easy");
+const btnAdvanced = document.querySelector("#advanced");
+const displayAdvanced = document.querySelector("#display-advanced");
+const btnExtreme = document.querySelector("#extreme");
+const displayExtreme = document.querySelector("#display-extreme");
+
+let fieldsCount;
+const previousGame = document.querySelector("#previous-game");
+let saved = false;
+btnContinue.disabled = true;
+const footer = document.querySelector("footer")
+
+// oldal betoltesekor local storageos cuccok jok legyenek
+if (JSON.parse(localStorage.getItem("time")) !== null) { // ha van mentett jatek
+  previousGame.innerHTML = `${JSON.parse(localStorage.getItem("modeText"))}: ${JSON.parse(localStorage.getItem("name"))} - ${JSON.parse(localStorage.getItem("time"))} másodperc`
+  btnContinue.disabled = false;
+}
+if (JSON.parse(localStorage.getItem("results")) !== null) { // ha vannak korabbi eredmenyek
+  btnDelete.style.display = "block";
+  let results = JSON.parse(localStorage.getItem("results"));
+  document.querySelector("#results").innerHTML = results.join("<br />");
+}
+
+// start
+btnEasy.addEventListener("click", () => {
+  isEasy = true;
+  isAdvanced = false;
+  isExtreme = false;
+
+  fieldsCount = 49;
+  displayGameSelector.style.display = "none";
+  displayGameField.style.display = "block";
+  displayEasy.style.display = "block";
+  displayAdvanced.style.display = "none";
+  displayExtreme.style.display = "none";
+  text.innerHTML = "Könnyű 7x7-es pálya"
+
+  setAcvtive(easyTable)
+  setInacvtive(advancedTable)
+  setInacvtive(extremeTable)
+
+  btnSave.style.display = "inline";
+  footer.style.display = "none";
+  nickname.innerHTML = document.getElementById("name").value === "" ? "Anonymous" : document.getElementById("name").value;
+  saved = false
+  startTimer();
+});
+
+btnAdvanced.addEventListener("click", () => {
+  isEasy = false;
+  isAdvanced = true;
+  isExtreme = false;
+
+  fieldsCount = 49;
+  displayGameSelector.style.display = "none";
+  displayGameField.style.display = "block";
+  displayEasy.style.display = "none";
+  displayAdvanced.style.display = "block";
+  displayExtreme.style.display = "none";
+  text.innerHTML = "Haladó 7x7-es pálya"
+
+  setInacvtive(easyTable)
+  setAcvtive(advancedTable)
+  setInacvtive(extremeTable)
+
+  btnSave.style.display = "inline";
+  footer.style.display = "none";
+  nickname.innerHTML = document.getElementById("name").value === "" ? "Anonymous" : document.getElementById("name").value;
+  saved = false;
+  startTimer();
+});
+
+btnExtreme.addEventListener("click", () => {
+  isEasy = false;
+  isAdvanced = false;
+  isExtreme = true;
+
+  fieldsCount = 100;
+  displayGameSelector.style.display = "none";
+  displayGameField.style.display = "block";
+  displayEasy.style.display = "none";
+  displayAdvanced.style.display = "none";
+  displayExtreme.style.display = "block";
+  text.innerHTML = "Extrém 10x10-es pálya"
+
+  setInacvtive(easyTable)
+  setInacvtive(advancedTable)
+  setAcvtive(extremeTable)
+
+  btnSave.style.display = "inline";
+  footer.style.display = "none";
+  nickname.innerHTML = document.getElementById("name").value === "" ? "Anonymous" : document.getElementById("name").value;
+  saved = false
+  startTimer();
+});
+
+// save
+btnSave.addEventListener("click", () => {
+  const sec = currentTime;
+  let data = null;
+  let mode = null;
+  let modeText = "";
+  if (isEasy) {
+    data = document.getElementById("table-easy")
+    mode = "easy"
+    modeText = "Könnyű 7x7-es pálya"
+  } else if (isAdvanced) {
+    data = document.getElementById("table-advanced")
+    mode = "advanced"
+    modeText = "Haladó 7x7-es pálya"
+  } else {
+    data = document.getElementById("table-extreme")
+    mode = "extreme"
+    modeText = "Extrém 10x10-es pálya"
+  }
+  localStorage.setItem("table", JSON.stringify(data.innerHTML));
+  localStorage.setItem("mode", JSON.stringify(mode))
+  localStorage.setItem("modeText", JSON.stringify(modeText))
+  localStorage.setItem("name", JSON.stringify(nickname.innerHTML));
+  localStorage.setItem("time", JSON.stringify(sec));
+
+  // jatek folytatasa szoveg frissitese
+  previousGame.innerHTML = `${modeText}: ${nickname.innerHTML} - ${sec} másodperc`
+  btnContinue.disabled = false;
+});
+
+// restart
+btnRestart.addEventListener("click", () => { 
+  reset();
+  btnSave.style.display = "inline";
+  startTimer();
+});
+
+// exit
+btnExit.addEventListener("click", () => {
+  displayGameField.style.display = "none";
+  displayGameSelector.style.display = "block";
+  footer.style.display = "block"
+  reset();
+});
+
+function reset() {
+  stopTimer();
+  winText.style.display = "none"
+
+  let rows = [];
+    myTable = tables.filter(e => !e.classList.contains("inactive"))[0]
+    rows = [...myTable.firstChild.nextSibling.childNodes];
+    rows = rows.filter((myTable, i) => i % 2 === 0); // osszes tr egy tombben
+
+  let cells = [];
+  rows.forEach((e) => {
+    if (e.firstChild !== null && e.firstChild.nextSibling !== null) {
+      e = e.firstChild.nextSibling;
+      cells.push(e); // sor 1 cellajanak berakasa
+      while (e.nextSibling !== null && e.nextSibling.nextSibling !== null) {
+        e = e.nextSibling.nextSibling;
+        cells.push(e); // sor tobbi cellajanak berakasa
+      }
+    }
+  });
+
+  cells.map((e) => {
+    // visszaallitas
+    if (e.classList.contains("black")) {
+      if (e.innerHTML === "0") e.style.color = "";
+      else if (e.innerHTML !== "") e.style.color = "red";
+    } else {
+      e.classList.remove("wrong");
+      e.classList.remove("light");
+      e.innerHTML = "";
+    }
+  });
+}
+
+function inacvtive(table) {
+  let rows = [...table.firstChild.nextSibling.childNodes];
+  rows = rows.filter((table, i) => i % 2 === 0); // osszes tr egy tombben
+
+  let cells = [];
+  rows.forEach((e) => {
+    if (e.firstChild !== null && e.firstChild.nextSibling !== null) {
+      e = e.firstChild.nextSibling;
+      cells.push(e); // sor 1 cellajanak berakasa
+      while (e.nextSibling !== null && e.nextSibling.nextSibling !== null) {
+        e = e.nextSibling.nextSibling;
+        cells.push(e); // sor tobbi cellajanak berakasa
+      }
+    }
+  });
+
+  cells.map(e => e.classList.add("inactive"));
+}
+
+function setInacvtive(table) {
+  let cells = getCells(table)
+  cells.map(e => e.classList.add("inactive"));
+  table.classList.add("inactive")
+}
+
+function setAcvtive(table) {
+  let cells = getCells(table)
+  cells.map(e => e.classList.remove("inactive"));
+  table.classList.remove("inactive")
+}
+
+
+function getCells(table) {
+  let rows = [...table.firstChild.nextSibling.childNodes];
+  rows = rows.filter((table, i) => i % 2 === 0); // osszes tr egy tombben
+
+  let cells = [];
+  rows.forEach((e) => {
+    if (e.firstChild !== null && e.firstChild.nextSibling !== null) {
+      e = e.firstChild.nextSibling;
+      cells.push(e); // sor 1 cellajanak berakasa
+      while (e.nextSibling !== null && e.nextSibling.nextSibling !== null) {
+        e = e.nextSibling.nextSibling;
+        cells.push(e); // sor tobbi cellajanak berakasa
+      }
+    }
+  });
+
+  return cells
+}
+
+function startTimer() {
+  time.innerHTML = currentTime
+  timer = setInterval(function () {
+    time.innerHTML = ++currentTime
+  }, 1000);
+}
+
+function stopTimer() {
+  currentTime = 0;
+  clearInterval(timer);
+}
+
+// folytatas
+document.getElementById("continue").addEventListener("click", () => {
+  const mode = JSON.parse(localStorage.getItem("mode"));
+  const savedTable = JSON.parse(localStorage.getItem("table"))
+
+  if (mode === "easy") {
+    isEasy = true;
+    isAdvanced = false;
+    isExtreme = false;
+    fieldsCount = 49;
+    displayGameSelector.style.display = "none";
+    displayGameField.style.display = "block";
+    displayEasy.style.display = "block";
+    displayAdvanced.style.display = "none";
+    displayExtreme.style.display = "none";
+    text.innerHTML = "Könnyű 7x7-es pálya"
+    setAcvtive(easyTable)
+    setInacvtive(advancedTable)
+    setInacvtive(extremeTable)
+    easyTable.innerHTML = savedTable
+  } else if (mode === "advanced") {
+    isEasy = false;
+    isAdvanced = true;
+    isExtreme = false;
+    fieldsCount = 49;
+    displayGameSelector.style.display = "none";
+    displayGameField.style.display = "block";
+    displayEasy.style.display = "none";
+    displayAdvanced.style.display = "block";
+    displayExtreme.style.display = "none";
+    text.innerHTML = "Haladó 7x7-es pálya"
+    setInacvtive(easyTable)
+    setAcvtive(advancedTable)
+    setInacvtive(extremeTable)
+    advancedTable.innerHTML = savedTable
+  } else {
+    isEasy = false;
+    isAdvanced = false;
+    isExtreme = true;
+    fieldsCount = 100;
+    displayGameSelector.style.display = "none";
+    displayGameField.style.display = "block";
+    displayEasy.style.display = "none";
+    displayAdvanced.style.display = "none";
+    displayExtreme.style.display = "block";
+    text.innerHTML = "Extrém 10x10-es pálya"
+    setInacvtive(easyTable)
+    setInacvtive(advancedTable)
+    setAcvtive(extremeTable)
+    extremeTable.innerHTML = savedTable
+  }
+
+  footer.style.display = "none"
+  saved = true
+  nickname.innerHTML = JSON.parse(localStorage.getItem("name"));
+  currentTime = JSON.parse(localStorage.getItem("time"));
+  startTimer();
+})
+
+// local storage korabbi eredmenyek uritese
+btnDelete.addEventListener("click", () => {
+  if (confirm("Biztosan törölni szeretnéd a korábbi eredményeket?")) {
+    localStorage.removeItem("results")
+    document.querySelector("#results").innerHTML = "<i>Nincsenek korábbi eredmények.</i>"
+    btnDelete.style.display = "none"
+  } 
+})
