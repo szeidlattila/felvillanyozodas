5f06d684a2930485fffe87ed1d747148ea656089
Szeidl Attila <a22oy5@inf.elte.hu>
2024-02-24 12:46:41 +0100

    refactor gameSelector.js

--- js/gameSelector.js
+++ js/gameSelector.js
@@ -1,9 +1,6 @@
 const displayGameSelector = document.querySelector("#display-game-selector");
 const displayGameField = document.querySelector("#display-game-field");
 const tables = [...document.querySelectorAll("table")];
-const easyTable = document.querySelector("#table-easy");
-const advancedTable = document.querySelector("#table-advanced");
-const extremeTable = document.querySelector("#table-extreme")
 
 const text = document.querySelector("#text")
 const nickname = document.querySelector("#nickname")
@@ -11,29 +8,26 @@ const winText = document.querySelector("#win")
 let time = document.querySelector("#time")
 let currentTime = 0;
 let timer = null
+let currentDifficulty = null
+const texts = {
+  easy: "Könnyű 7x7-es pálya",
+  advanced: "Haladó 7x7-es pálya",
+  extreme: "Extrém 10x10-es pálya",
+}
+const difficulties = ["easy", "advanced", "extreme"]
 
+const btns = [...document.querySelectorAll(".difficulty")];
 const btnSave = document.querySelector("#save");
 const btnRestart = document.querySelector("#restart");
 const btnExit = document.querySelector("#exit");
 const btnContinue = document.querySelector("#continue-btn")
 const btnDelete = document.querySelector("#delete-results")
-
-let isEasy = false;
-let isAdvanced = false;
-let isExtreme = false;
-
-const btnEasy = document.querySelector("#easy");
-const displayEasy = document.querySelector("#display-easy");
-const btnAdvanced = document.querySelector("#advanced");
-const displayAdvanced = document.querySelector("#display-advanced");
-const btnExtreme = document.querySelector("#extreme");
-const displayExtreme = document.querySelector("#display-extreme");
+const previousGame = document.querySelector("#previous-game");
+const footer = document.querySelector("footer")
 
 let fieldsCount;
-const previousGame = document.querySelector("#previous-game");
 let saved = false;
 btnContinue.disabled = true;
-const footer = document.querySelector("footer")
 
 // local storage
 if (JSON.parse(localStorage.getItem("time")) !== null) { // ha van mentett jatek
@@ -47,105 +41,37 @@ if (JSON.parse(localStorage.getItem("results")) !== null) { // ha vannak korabbi
 }
 
 // start
-btnEasy.addEventListener("click", () => {
-  isEasy = true;
-  isAdvanced = false;
-  isExtreme = false;
+btns.map((btn) => btn.addEventListener("click", (e) => {
+  let difficulty = e.target.id;
+  currentDifficulty = difficulty
 
-  fieldsCount = 49;
+  fieldsCount = difficulty === "extreme" ? 100 : 49;
   displayGameSelector.style.display = "none";
   displayGameField.style.display = "block";
-  displayEasy.style.display = "block";
-  displayAdvanced.style.display = "none";
-  displayExtreme.style.display = "none";
-  text.innerHTML = "Könnyű 7x7-es pálya"
+  document.querySelector("#display-" + difficulty).style.display = "block";
+  difficulties.filter((diff) => diff !== difficulty).map((diff) => document.querySelector("#display-" + diff).style.display = "none");
+  text.innerHTML = texts[difficulty]
 
-  setAcvtive(easyTable)
-  setInacvtive(advancedTable)
-  setInacvtive(extremeTable)
+  setActive(document.querySelector("#table-" + difficulty))
+  difficulties.filter((diff) => diff !== difficulty).map((diff) => setInactive(document.querySelector("#table-" + diff)))
 
   btnSave.style.display = "inline";
   footer.style.display = "none";
-  nickname.innerHTML = document.getElementById("name").value === "" ? "Anonymous" : document.getElementById("name").value;
+  nickname.innerHTML = document.querySelector("#name").value === "" ? "Anonymous" : document.querySelector("#name").value;
   saved = false
   startTimer();
-});
-
-btnAdvanced.addEventListener("click", () => {
-  isEasy = false;
-  isAdvanced = true;
-  isExtreme = false;
-
-  fieldsCount = 49;
-  displayGameSelector.style.display = "none";
-  displayGameField.style.display = "block";
-  displayEasy.style.display = "none";
-  displayAdvanced.style.display = "block";
-  displayExtreme.style.display = "none";
-  text.innerHTML = "Haladó 7x7-es pálya"
-
-  setInacvtive(easyTable)
-  setAcvtive(advancedTable)
-  setInacvtive(extremeTable)
-
-  btnSave.style.display = "inline";
-  footer.style.display = "none";
-  nickname.innerHTML = document.getElementById("name").value === "" ? "Anonymous" : document.getElementById("name").value;
-  saved = false;
-  startTimer();
-});
-
-btnExtreme.addEventListener("click", () => {
-  isEasy = false;
-  isAdvanced = false;
-  isExtreme = true;
-
-  fieldsCount = 100;
-  displayGameSelector.style.display = "none";
-  displayGameField.style.display = "block";
-  displayEasy.style.display = "none";
-  displayAdvanced.style.display = "none";
-  displayExtreme.style.display = "block";
-  text.innerHTML = "Extrém 10x10-es pálya"
-
-  setInacvtive(easyTable)
-  setInacvtive(advancedTable)
-  setAcvtive(extremeTable)
-
-  btnSave.style.display = "inline";
-  footer.style.display = "none";
-  nickname.innerHTML = document.getElementById("name").value === "" ? "Anonymous" : document.getElementById("name").value;
-  saved = false
-  startTimer();
-});
+}));
 
 // save
 btnSave.addEventListener("click", () => {
-  const sec = currentTime;
-  let data = null;
-  let mode = null;
-  let modeText = "";
-  if (isEasy) {
-    data = document.getElementById("table-easy")
-    mode = "easy"
-    modeText = "Könnyű 7x7-es pálya"
-  } else if (isAdvanced) {
-    data = document.getElementById("table-advanced")
-    mode = "advanced"
-    modeText = "Haladó 7x7-es pálya"
-  } else {
-    data = document.getElementById("table-extreme")
-    mode = "extreme"
-    modeText = "Extrém 10x10-es pálya"
-  }
-  localStorage.setItem("table", JSON.stringify(data.innerHTML));
-  localStorage.setItem("mode", JSON.stringify(mode))
-  localStorage.setItem("modeText", JSON.stringify(modeText))
+  localStorage.setItem("table", JSON.stringify(document.querySelector("#table-" + currentDifficulty).innerHTML));
+  localStorage.setItem("mode", JSON.stringify(currentDifficulty))
+  localStorage.setItem("modeText", JSON.stringify(texts[currentDifficulty]))
   localStorage.setItem("name", JSON.stringify(nickname.innerHTML));
-  localStorage.setItem("time", JSON.stringify(sec));
+  localStorage.setItem("time", JSON.stringify(currentTime));
 
   // jatek folytatasa szoveg frissitese
-  previousGame.innerHTML = `${modeText}: ${nickname.innerHTML} - ${sec} másodperc`
+  previousGame.innerHTML = `${texts[currentDifficulty]}: ${nickname.innerHTML} - ${currentTime} másodperc`
   btnContinue.disabled = false;
 });
 
@@ -198,32 +124,13 @@ function reset() {
   });
 }
 
-function inacvtive(table) {
-  let rows = [...table.firstChild.nextSibling.childNodes];
-  rows = rows.filter((table, i) => i % 2 === 0); // osszes tr egy tombben
-
-  let cells = [];
-  rows.forEach((e) => {
-    if (e.firstChild !== null && e.firstChild.nextSibling !== null) {
-      e = e.firstChild.nextSibling;
-      cells.push(e); // sor 1 cellajanak berakasa
-      while (e.nextSibling !== null && e.nextSibling.nextSibling !== null) {
-        e = e.nextSibling.nextSibling;
-        cells.push(e); // sor tobbi cellajanak berakasa
-      }
-    }
-  });
-
-  cells.map(e => e.classList.add("inactive"));
-}
-
-function setInacvtive(table) {
+function setInactive(table) {
   let cells = getCells(table)
   cells.map(e => e.classList.add("inactive"));
   table.classList.add("inactive")
 }
 
-function setAcvtive(table) {
+function setActive(table) {
   let cells = getCells(table)
   cells.map(e => e.classList.remove("inactive"));
   table.classList.remove("inactive")
@@ -232,7 +139,7 @@ function setAcvtive(table) {
 
 function getCells(table) {
   let rows = [...table.firstChild.nextSibling.childNodes];
-  rows = rows.filter((table, i) => i % 2 === 0); // osszes tr egy tombben
+  rows = rows.filter((_, i) => i % 2 === 0); // osszes tr egy tombben
 
   let cells = [];
   rows.forEach((e) => {
@@ -262,56 +169,20 @@ function stopTimer() {
 }
 
 // folytatas
-document.getElementById("continue").addEventListener("click", () => {
+document.querySelector("#continue").addEventListener("click", () => {
   const mode = JSON.parse(localStorage.getItem("mode"));
   const savedTable = JSON.parse(localStorage.getItem("table"))
 
-  if (mode === "easy") {
-    isEasy = true;
-    isAdvanced = false;
-    isExtreme = false;
-    fieldsCount = 49;
-    displayGameSelector.style.display = "none";
-    displayGameField.style.display = "block";
-    displayEasy.style.display = "block";
-    displayAdvanced.style.display = "none";
-    displayExtreme.style.display = "none";
-    text.innerHTML = "Könnyű 7x7-es pálya"
-    setAcvtive(easyTable)
-    setInacvtive(advancedTable)
-    setInacvtive(extremeTable)
-    easyTable.innerHTML = savedTable
-  } else if (mode === "advanced") {
-    isEasy = false;
-    isAdvanced = true;
-    isExtreme = false;
-    fieldsCount = 49;
-    displayGameSelector.style.display = "none";
-    displayGameField.style.display = "block";
-    displayEasy.style.display = "none";
-    displayAdvanced.style.display = "block";
-    displayExtreme.style.display = "none";
-    text.innerHTML = "Haladó 7x7-es pálya"
-    setInacvtive(easyTable)
-    setAcvtive(advancedTable)
-    setInacvtive(extremeTable)
-    advancedTable.innerHTML = savedTable
-  } else {
-    isEasy = false;
-    isAdvanced = false;
-    isExtreme = true;
-    fieldsCount = 100;
-    displayGameSelector.style.display = "none";
-    displayGameField.style.display = "block";
-    displayEasy.style.display = "none";
-    displayAdvanced.style.display = "none";
-    displayExtreme.style.display = "block";
-    text.innerHTML = "Extrém 10x10-es pálya"
-    setInacvtive(easyTable)
-    setInacvtive(advancedTable)
-    setAcvtive(extremeTable)
-    extremeTable.innerHTML = savedTable
-  }
+  currentDifficulty = mode
+  fieldsCount = mode === "extreme" ? 100 : 49;
+  displayGameSelector.style.display = "none";
+  displayGameField.style.display = "block";
+  document.querySelector("#display-" + mode).style.display = "block";
+  difficulties.filter((diff) => diff !== mode).map((diff) => document.querySelector("#display-" + diff).style.display = "none");
+  text.innerHTML = texts[mode]
+  setActive(document.querySelector("#table-" + mode))
+  difficulties.filter((diff) => diff !== mode).map((diff) => setInactive(document.querySelector("#table-" + diff)))
+  document.querySelector("#table-" + mode).innerHTML = savedTable
 
   footer.style.display = "none"
   saved = true
